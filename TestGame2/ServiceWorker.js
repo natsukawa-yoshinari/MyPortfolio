1cdda1d52ffd524f5faccad808513b49622d66d9
natsukawa-yoshinari <ttc2550dg0010@edu.tech.ac.jp>
2026-04-18 15:38:53 +0900

    Add TestGame2

---
+++ TestGame2/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-uni260418_clicker-1.0";
+const contentToCache = [
+    "Build/TestGame2.loader.js",
+    "Build/TestGame2.framework.js",
+    "Build/TestGame2.data",
+    "Build/TestGame2.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
