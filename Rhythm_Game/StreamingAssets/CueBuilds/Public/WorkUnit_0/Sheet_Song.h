1273e2e5b71238ef9ff60ba36cd9578cedc49793
natsukawa-yoshinari <ttc2550dg0010@edu.tech.ac.jp>
2026-06-25 00:56:26 +0900

    add rhythmGame

---
+++ Rhythm_Game/StreamingAssets/CueBuilds/Public/WorkUnit_0/Sheet_Song.h
@@ -0,0 +1,26 @@
+﻿/*===========================================================================*
+ *  Header file for Atom CueSheet Binary
+ *  Project          : 260620_MyRythmGame
+ *  Tool Ver.        : Ver.3.56.01
+ *  ACB  Ver.        : Ver.1.46.0
+ *  File Path        : C:/CRIWARE/CriAtomCraft/projects/260620_MyRythmGame/CueBuilds/Public/WorkUnit_0
+ *  File Name        : Sheet_Song.h
+ *  File Size        : 7392 bytes
+ *  Date Time        : 2026/06/23 18:39:34
+ *  Target           : Public
+ *  Cues             : 1
+ *  CueSheet Comment : 
+ *  Stream Awb Path  : C:/CRIWARE/CriAtomCraft/projects/260620_MyRythmGame/CueBuilds/Public/WorkUnit_0/Sheet_Song.awb
+ *===========================================================================*/
+
+#define CRI_SHEET_SONG_CUENUM (1)
+
+/* AISAC Control List (AISAC Control ID) */
+
+/* Cue List (Cue ID) */
+#define CRI_SHEET_SONG_SONG_SHININGSTAR     ( 0) /*  */
+
+/* Block List (Block Index) */
+
+/* end of file */
+
